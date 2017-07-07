146dd3ffb313a20bff488342de19b69e4afc3fa2
Jase Cutler <jasecutler@gmail.com>
2017-07-07 15:20:46 -0700

    Question 1 thru 5 done

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// Since the `data.js` file is loaded in your `index.html` before this one, 
+// Since the `data.js` file is loaded in your `index.html` before this one,
 // you have access to the `data` variable that contains the Etsy Store data.
 // Open your `index.html` page in your browser and you can verify that the following
 // is properly working. The `data` variable is an array with 25 items in it
@@ -9,6 +9,11 @@ console.log(data);
 // 1: Show me how to calculate the average price of all items.
 function question1 () {
   // Answer:
+  let average = 0;
+  for(i=0;i<data.length;i++) {
+    average += data[i].price
+  }
+  return average / data.length
 }
 
 
@@ -16,6 +21,13 @@ function question1 () {
 // 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
 function question2 () {
   // Answer:
+  let listProducts = [];
+  for(i=0;i<data.length;i++) {
+    if (data[i].price < 18 && data[i].price > 14) {
+      listProducts.push(data[i]);
+    }
+  }
+  return listProducts;
 }
 
 
@@ -23,19 +35,43 @@ function question2 () {
 // 3: Which item has a "GBP" currency code? Display it's name and price.
 function question3 () {
   // Answer:
+  let listProducts = [];
+  for(i=0;i<data.length;i++) {
+    if(data[i].currency_code == "GBP") {
+      listProducts.push({name:data[i].name, price:data[i].price, currency_code:data[i].currency_code})
+    }
+  }
+  return listProducts;
 }
 
 
 // 4: Display a list of all items who are made of wood.
 function question4 () {
   // Answer:
+  let listProducts = [];
+  for(i=0;i<data.length;i++) {
+    for(j=0;j<data[i].materials.length;j++){
+      if(data[i].materials[j]=="wood"){
+        listProducts.push({name:data[i].name, materials:data[i].materials});
+      }
+    }
+  }
+  return listProducts;
 }
 
 
-// 5: Which items are made of eight or more materials? 
+// 5: Which items are made of eight or more materials?
 //    Display the name, number of items and the items it is made of.
 function question5 () {
   // Answer:
+  let listProducts = [];
+  for(i=0;i<data.length;i++) {
+      if(data[i].materials.length>=8){
+        data[i].materials_number = data[i].materials.length;
+        listProducts.push({name:data[i].name, materials_number:data[i].materials_number, materials:data[i].materials});
+    }
+  }
+  return listProducts;
 }
 
 
@@ -43,4 +79,5 @@ function question5 () {
 // Answer:
 function question6 () {
   // Answer:
+
 }
